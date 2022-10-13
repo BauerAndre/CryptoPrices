b8f59ab1bcaa5ea1e9c0e42c585b7043dd9e35b7
Andre Bauer <andrebauer@live.com>
2022-10-13 13:50:48 +0900

    react navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from './screens/Home';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
