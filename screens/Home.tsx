0519fb37c641db707550c8cd10dd79245b5ce41a
Andre Bauer <andrebauer@live.com>
2022-10-13 14:30:54 +0900

    navigation

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Pressable} from 'react-native';
 
-export const HomeScreen = () => {
+export const HomeScreen = ({navigation}: {navigation: any}) => {
+  const navigateToDetail = () => {
+    navigation.navigate('Detail');
+  };
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Pressable onPress={navigateToDetail}>
+        <Text>Navigate to Detail</Text>
+      </Pressable>
     </View>
   );
 };
