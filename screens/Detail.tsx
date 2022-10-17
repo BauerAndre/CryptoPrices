3cfd0b1dea5898869556aaff571eaf92b4999cd7
Andre Bauer <andrebauer@live.com>
2022-10-17 15:27:03 +0900

    detail page

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
+import axios from 'axios';
 
 export const DetailScreen = ({route}: {route: any}) => {
   const id = route.params.id;
+  const [cryptoProfile, setCryptoProfile] = useState();
+  const [cryptoMarketData, setCryptoMarketData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/cryptos/market-data/${id}`)
+      .then(response => {
+        setCryptoMarketData(response.data);
+      });
+
+    axios.get(`http://localhost:3000/cryptos/profile/${id}`).then(response => {
+      setCryptoProfile(response.data);
+    });
+  }, []);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>{id}</Text>
+      <Text>{cryptoProfile}</Text>
     </View>
   );
 };
