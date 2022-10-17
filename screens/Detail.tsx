495f048560631d2c9ed23bf6a881549746b36adb
Andre Bauer <andrebauer@live.com>
2022-10-17 15:50:58 +0900

    calls combined

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
 import axios from 'axios';
+import { API_URL } from '../consts/app-costs';
 
 export const DetailScreen = ({route}: {route: any}) => {
   const id = route.params.id;
@@ -8,20 +9,18 @@ export const DetailScreen = ({route}: {route: any}) => {
   const [cryptoMarketData, setCryptoMarketData] = useState();
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:3000/cryptos/market-data/${id}`)
-      .then(response => {
-        setCryptoMarketData(response.data);
-      });
-
-    axios.get(`http://localhost:3000/cryptos/profile/${id}`).then(response => {
-      setCryptoProfile(response.data);
+    Promise.all([
+      axios.get(`${API_URL}/cryptos/market-data/${id}`),
+      axios.get(`${API_URL}/cryptos/profile/${id}`),
+    ]).then(([resMarketData, resProfile]) => {
+      setCryptoMarketData(resMarketData.data);
+      setCryptoProfile(resProfile.data);
     });
   }, []);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>{cryptoProfile}</Text>
+      <Text>{JSON.stringify(cryptoProfile)}</Text>
     </View>
   );
 };
